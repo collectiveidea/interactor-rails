a2cd599b9a7b399f30ba072e5719c918944fcf87
Steve Richert <steve.richert@gmail.com>
2013-08-15 15:11:44 -0400

    Generate interactor-rails gem with Bundler

---
+++ lib/interactor/rails.rb
@@ -0,0 +1,7 @@
+require "interactor/rails/version"
+
+module Interactor
+  module Rails
+    # Your code goes here...
+  end
+end
---
+++ lib/interactor/rails/version.rb
@@ -0,0 +1,5 @@
+module Interactor
+  module Rails
+    VERSION = "0.0.1"
+  end
+end
